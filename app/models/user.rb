9b009b629ecff5c579f8c2ab985b18003cd4f794
unknown <christianp1894gmail.com>
2020-06-07 16:02:21 -0500

    Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+    validates :username, presence: true
+    validates :password, presence: true
+    validates :email, presence: true
+
+    has_many :posts
+    has_many :comments
+end
